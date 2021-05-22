de06998e8a6564ad5d281c7ae2d019212b87b63c
Mathis Kretz <mathis@bespinian.io>
2021-05-22 23:42:39 +0200

    Fix tracepoint name

--- playground-this/exitcodes/ebpf/bpf/sched_process_exit.c
+++ playground-this/exitcodes/ebpf/bpf/sched_process_exit.c
@@ -3,7 +3,7 @@
 
 char __license[] SEC("license") = "Dual MIT/GPL";
 
-SEC("tracepoint/syscalls/sys_process_exit")
+SEC("tracepoint/sched/sched_process_exit")
 int bpf_prog(void *ctx) {
   char msg[] = "Process exited";
   bpf_trace_printk(msg, sizeof(msg));
