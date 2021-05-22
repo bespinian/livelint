b3f23977f62ae104339510771dcd4e182d8bfc4c
Mathis Kretz <mathis@bespinian.io>
2021-05-22 23:26:54 +0200

    Try to install ebpf in kernel

--- playground-this/exitcodes/ebpf/main.go
+++ playground-this/exitcodes/ebpf/main.go
@@ -1,6 +1,42 @@
 package main
 
+import (
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/cilium/ebpf/link"
+	"golang.org/x/sys/unix"
+)
+
 //go:generate go run github.com/cilium/ebpf/cmd/bpf2go -cc clang-11 SchedProcessExit ./bpf/sched_process_exit.c -- -I../headers
 
 func main() {
+	// Subscribe to signals for terminating the program.
+	stopper := make(chan os.Signal, 1)
+	signal.Notify(stopper, os.Interrupt, syscall.SIGTERM)
+
+	// Increase the rlimit of the current process to provide sufficient space
+	// for locking memory for the eBPF map.
+	if err := unix.Setrlimit(unix.RLIMIT_MEMLOCK, &unix.Rlimit{
+		Cur: unix.RLIM_INFINITY,
+		Max: unix.RLIM_INFINITY,
+	}); err != nil {
+		log.Fatalf("failed to set temporary rlimit: %v", err)
+	}
+
+	// Load pre-compiled programs and maps into the kernel.
+	objs := SchedProcessExitObjects{}
+	if err := LoadSchedProcessExitObjects(&objs, nil); err != nil {
+		log.Fatalf("loading objects: %v", err)
+	}
+	defer objs.Close()
+
+	tp, err := link.Tracepoint("syscalls", "sched_process_exit", objs.BpfProg)
+	if err != nil {
+		log.Fatalf("opening tracepoint: %s", err)
+	}
+	defer tp.Close()
+
 }
