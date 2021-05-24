d0876552aa64a67f4e3b97ed2998e2b3fb809ede
Mathis Kretz <mathis@bespinian.io>
2021-05-24 21:58:24 +0200

    Add exit code to trace output

--- playground-this/exitcodes/ebpf/bpf/sched_process_exit.c
+++ playground-this/exitcodes/ebpf/bpf/sched_process_exit.c
@@ -1,3 +1,5 @@
+#define BPF_NO_PRESERVE_ACCESS_INDEX 0
+#include "vmlinux.h"
 #include "common.h"
 #include "bpf_helpers.h"
 
@@ -5,6 +7,7 @@ char __license[] SEC("license") = "Dual MIT/GPL";
 
 struct event_t {
         u32 pid;
+        int ec;
 };
 
 struct {
@@ -12,9 +15,11 @@ struct {
 } events SEC(".maps");
 
 SEC("tracepoint/sched/sched_process_exit")
-int bpf_prog(void *ctx) {
+int bpf_prog(struct context* ctx) {
   struct event_t event;
   event.pid = bpf_get_current_pid_tgid();
+  struct task_struct *task = (typeof(task))bpf_get_current_task();
+  event.ec = task->exit_code;
   bpf_perf_event_output(ctx, &events, BPF_F_CURRENT_CPU, &event, sizeof(event));
   return 0;
 }
