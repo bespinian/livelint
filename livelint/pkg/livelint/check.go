5461c030e9a148774ecd4e102f88e51ce4417f0b
Gabriel Koch <gabriel@bespinian.io>
2021-07-29 17:30:53 +0200

    Add check for image pull errors

--- livelint/pkg/livelint/check.go
+++ livelint/pkg/livelint/check.go
@@ -1,6 +1,8 @@
 package livelint
 
-import "fmt"
+import (
+	"fmt"
+)
 
 // Topo lists the topology of a connection path.
 func (n *livelint) Check(namespace, deploymentName string) error {
@@ -33,23 +35,34 @@ func (n *livelint) Check(namespace, deploymentName string) error {
 		return fmt.Errorf("error getting pods: %w", err)
 	}
 	
-	for i := 0; i < len(allPods); i++ {
-		pod := allPods[i]
-
+	for _, pod := range allPods {
 		n.checkPodConditions(pod)
 		
-		nonStartedContainerNames := n.getNonStartedContainerNames(pod)
+		nonStartedContainers := n.getNonStartedContainers(pod)
 
-		if len(nonStartedContainerNames) > 0 {
-			fmt.Printf("NOK: There are %d containers that are not started in pod %s\n", len(nonStartedContainerNames), pod.Name)
-			fmt.Printf("Trying to print logs from the first non started container %s\n", nonStartedContainerNames[0])
+		if len(nonStartedContainers) > 0 {
+			fmt.Printf("NOK: There are %d containers that are not started in pod %s\n", len(nonStartedContainers), pod.Name)
+			fmt.Printf("Trying to print logs from the first non started container %s\n", nonStartedContainers[0].Name)
 			
-			logs, err := n.checkContainerLogs(pod, nonStartedContainerNames[0])
+			logs, err := n.checkContainerLogs(pod, nonStartedContainers[0].Name)
 			if (err == nil)  {
 				fmt.Println(*logs)
 				return nil
 			} else {
 				fmt.Println("Could not get container logs")
+			}
+
+			for _, container := range nonStartedContainers {
+				hasImagePullError, reason, message := n.checkImagePullErrors(pod, container.Name)
+				if (hasImagePullError) {
+					fmt.Printf("Container %s has error pulling image (%s): %s\n",
+						container.Name,
+						container.Image,
+						reason,
+						)
+					fmt.Println(message)
+					fmt.Println("Verify that the image name, tag and registry are correct and that credentials are correct.")
+				}
 			}
 		}
 	}
