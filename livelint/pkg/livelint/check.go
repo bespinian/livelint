cf60d1ed728b298590d7f071698e9aa75e5e021c
Gabriel Koch <gabriel@bespinian.io>
2021-07-29 15:06:43 +0200

    Refactor log checking

--- livelint/pkg/livelint/check.go
+++ livelint/pkg/livelint/check.go
@@ -23,7 +23,6 @@ func (n *livelint) Check(namespace, deploymentName string) error {
 
 	if len(nonRunningPods) > 0 {
 		fmt.Println("NOK: There are non running pods")
-		return nil
 
 		} else {
 		fmt.Println("OK: All pods running")	
@@ -40,21 +39,13 @@ func (n *livelint) Check(namespace, deploymentName string) error {
 
 		if len(nonStartedContainerNames) > 0 {
 			fmt.Printf("NOK: There are %d containers that are not started in pod %s\n", len(nonStartedContainerNames), pod.Name)
-			fmt.Printf("Trying to print logs from the first container %s\n", nonStartedContainerNames[0])
-
-			logs, err := n.tailPodLogs(namespace, pod.Name, nonStartedContainerNames[0], 20, false)
-
-			if err != nil {
-				fmt.Printf("error getting logs: %s", err.Error());
-				fmt.Println("Trying logs of previous pod");
-				logs, err = n.tailPodLogs(namespace, pod.Name, nonStartedContainerNames[0], 20, true)
-				if err != nil {
-					return fmt.Errorf("error getting logs: %w", err)
-				}
-			}
-
-			if (len(logs) > 0) {
-				fmt.Println(logs)
+			fmt.Printf("Trying to print logs from the first non started container %s\n", nonStartedContainerNames[0])
+			
+			logs, err := n.checkContainerLogs(pod, nonStartedContainerNames[0])
+			if (err != nil)  {
+				fmt.Printf("error getting container logs: %s", err.Error())
+			} else {
+				fmt.Println(*logs)
 				return nil
 			}
 		}
