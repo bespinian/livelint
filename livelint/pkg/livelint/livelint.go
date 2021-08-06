ad6bdbae311cf61f99402b15197adc03eb16bc9c
Lena Fuhrimann <lena.fuhrimann@protonmail.com>
2021-08-06 09:03:30 +0200

    Add colors and make tool less verbose

--- livelint/pkg/livelint/livelint.go
+++ livelint/pkg/livelint/livelint.go
@@ -4,14 +4,14 @@ import "k8s.io/client-go/kubernetes"
 
 // Livelint represents a livelint application.
 type Livelint interface {
-	Check(namespace, deploymentName string) error
+	Check(namespace, deploymentName string, isVerbose bool) error
 }
 
 type livelint struct {
 	k8s kubernetes.Interface
 }
 
-// New creates a net3 application.
+// New creates a livelint application.
 func New(k8s kubernetes.Interface) Livelint {
 	l := &livelint{
 		k8s: k8s,
