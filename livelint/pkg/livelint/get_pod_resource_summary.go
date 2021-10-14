b5c3b73b39783bf97a722f4c49a13e9cfd19c32c
Gabriel Koch <7779684+elessar-ch@users.noreply.github.com>
2021-10-14 21:43:46 +0200

    Add check for "is the cluster full"

---
+++ livelint/pkg/livelint/get_pod_resource_summary.go
@@ -0,0 +1,24 @@
+package livelint
+
+import (
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
+)
+
+// getPodResourceSummary returns summary of a pods cpu and memory requests.
+// Returns (cpu requests, memory requests).
+func getPodResourceSummary(pod corev1.Pod) (*resource.Quantity, *resource.Quantity) {
+	podTotalCPURequests := resource.NewDecimalQuantity(*resource.Zero.AsDec(), resource.DecimalSI)
+	podTotalMemoryRequests := resource.NewDecimalQuantity(*resource.Zero.AsDec(), resource.BinarySI)
+	for _, container := range pod.Spec.Containers {
+		cpuRequests := container.Resources.Requests.Cpu()
+		if cpuRequests != nil {
+			podTotalCPURequests.Add((*cpuRequests))
+		}
+		memoryRequests := container.Resources.Requests.Memory()
+		if memoryRequests != nil {
+			podTotalMemoryRequests.Add((*memoryRequests))
+		}
+	}
+	return podTotalCPURequests, podTotalMemoryRequests
+}
