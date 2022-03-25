48aecbc1aa76589ccb4bc53f0423ed1258701376
Gabriel Koch <7779684+elessar-ch@users.noreply.github.com>
2022-03-25 13:52:05 +0100

    Add check for port mismatch

--- livelint/pkg/livelint/check_target_port_matches_container_port.go
+++ livelint/pkg/livelint/check_target_port_matches_container_port.go
@@ -1,18 +1,54 @@
 package livelint
 
-func checkTargetPortMatchesContainerPort() CheckResult {
-	yes := askUserYesOrNo("Is the targetPort on the Service matching the containerPort in the Pod?")
+import (
+	"context"
+	"fmt"
+	"log"
+	"strings"
 
-	if !yes {
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func (n *Livelint) checkTargetPortMatchesContainerPort(allPods []corev1.Pod, serviceName string, namespace string) CheckResult {
+	service, err := n.k8s.CoreV1().Services(namespace).Get(context.Background(), serviceName, metav1.GetOptions{})
+	if err != nil {
+		log.Fatal(fmt.Errorf("error getting service %s in namespace %s: %w", serviceName, namespace, err))
+	}
+
+	unexposedPorts := []string{}
+	firstPod := allPods[0]
+	for _, servicePort := range service.Spec.Ports {
+		portIsExposed := false
+		for _, container := range firstPod.Spec.Containers {
+			for _, containerPort := range container.Ports {
+				if containerPort.ContainerPort == servicePort.TargetPort.IntVal &&
+					containerPort.Protocol == servicePort.Protocol {
+					portIsExposed = true
+					break
+				}
+			}
+			if portIsExposed {
+				break
+			}
+		}
+		if !portIsExposed {
+			unexposedPorts = append(unexposedPorts, fmt.Sprintf("%s %d", servicePort.Protocol, servicePort.TargetPort.IntVal))
+		}
+	}
+
+	if len(unexposedPorts) > 0 {
+		portList := strings.Join(unexposedPorts, ", ")
+
 		return CheckResult{
 			HasFailed:    true,
-			Message:      "The targetPort on the Service doesn't match the containerPort in the Pod",
+			Message:      fmt.Sprintf("The targetPorts %s on the Service don't match the containerPort in the Pod", portList),
 			Instructions: "Fix the Service targetPort and the containerPort",
 		}
 	}
 
 	return CheckResult{
-		Message:      "The targetPort on the Service matches the containerPort in the Pod",
+		Message:      "The targetPorts on the Service match the containerPorts in the Pod",
 		Instructions: "The issue could be with the Kube Proxy",
 	}
 }
