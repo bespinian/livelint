effa72fce25d25a26d13afb1f37f92e655bfd061
Gabriel Koch <7779684+elessar-ch@users.noreply.github.com>
2022-04-22 15:50:13 +0200

    Update check flow replacing check for RunContainerError

---
+++ internal/livelint/check_is_container_creating.go
@@ -0,0 +1,24 @@
+package livelint
+
+import (
+	"fmt"
+
+	apiv1 "k8s.io/api/core/v1"
+)
+
+func (n *Livelint) checkIsContainerCreating(pod apiv1.Pod) CheckResult {
+	if pod.Status.Phase == apiv1.PodPending {
+		for _, containerStatus := range append(pod.Status.ContainerStatuses, pod.Status.InitContainerStatuses...) {
+			if containerStatus.State.Waiting != nil && containerStatus.State.Waiting.Reason == "ContainerCreating" {
+				return CheckResult{
+					HasFailed: true,
+					Message:   fmt.Sprintf("Container %s of pod %sis in state ContainerCreating", containerStatus.Name, pod.Name),
+				}
+			}
+		}
+	}
+
+	return CheckResult{
+		Message: "No Container is in state ContainerCreating",
+	}
+}
