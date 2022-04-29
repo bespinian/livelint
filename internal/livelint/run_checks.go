3be9d4d0f129f79030cdca29b133b9a39aac6c90
Gabriel Koch <7779684+elessar-ch@users.noreply.github.com>
2022-04-29 15:04:28 +0200

    Automatically detect and check matching services and ingresses

--- internal/livelint/run_checks.go
+++ internal/livelint/run_checks.go
@@ -169,52 +169,73 @@ func (n *Livelint) RunChecks(namespace, deploymentName string, isVerbose bool) e
 
 	n.tea.Send(summaryMsg("Pods are running correctly"))
 
-	service := n.askUserForService(namespace, deploymentName)
-	// Can you see a list of endpoints?
-	result = n.checkCanSeeEndpoints(service.Name, namespace)
-	n.tea.Send(stepMsg(result))
-	if result.HasFailed {
+	allServices, partlyMatchingServices, err := n.getServices(namespace, deploymentName)
+	if err != nil {
+		log.Fatal(fmt.Errorf("error getting services in namespace %s: %w", namespace, err))
+	}
 
-		// Is the Selector matching the Pod label?
-		result = n.checkIsSelectorMatchingPodLabel(allPods, service.Name, namespace)
+	allServices = append(allServices, partlyMatchingServices...)
+
+	if len(allServices) < 1 {
+		result = CheckResult{
+			HasFailed: true,
+			Message:   fmt.Sprintf("No services match the deployment's matchSelector."),
+		}
+		n.tea.Send(stepMsg(result))
+		return nil
+	}
+
+	for _, service := range allServices {
+
+		// Can you see a list of endpoints?
+		result = n.checkCanSeeEndpoints(service.Name, namespace)
+		n.tea.Send(stepMsg(result))
+		if result.HasFailed {
+			// Does the Pod have an IP address assigned?
+			result = checkPodHasIPAddressAssigned(allPods)
+			n.tea.Send(stepMsg(result))
+
+			return nil
+		}
+
+		// Can you visit the app?
+		result = n.checkCanVisitServiceApp(service)
+		n.tea.Send(stepMsg(result))
+		if result.HasFailed {
+
+			// Is the targetPort on the Service matching the containerPort in the Pod?
+			result = n.checkTargetPortMatchesContainerPort(allPods, service.Name, namespace)
+			n.tea.Send(stepMsg(result))
+
+			return nil
+		}
+
+		n.tea.Send(summaryMsg(fmt.Sprintf("The Service %s is running correctly", service.Name)))
+	}
+
+	ingresses, err := n.getIngressesFromServices(namespace, allServices)
+	if err != nil {
+		log.Fatal(fmt.Errorf("error getting ingresses in namespace %s: %w", namespace, err))
+	}
+
+	if len(ingresses) < 1 {
+		result = CheckResult{
+			HasFailed: true,
+			Message:   fmt.Sprintf("No ingresses match any of the previously detected services names and ports."),
+		}
+		n.tea.Send(stepMsg(result))
+		return nil
+	}
+
+	for _, ingress := range ingresses {
+		// Can you see a list of Backends?
+		result = n.checkCanSeeBackends(ingress, namespace)
 		n.tea.Send(stepMsg(result))
 		if result.HasFailed {
 			return nil
 		}
 
-		// Does the Pod have an IP address assigned?
-		result = checkPodHasIPAddressAssigned(allPods)
-		n.tea.Send(stepMsg(result))
-
-		return nil
-	}
-
-	// Can you visit the app?
-	result = n.checkCanVisitServiceApp(service)
-	n.tea.Send(stepMsg(result))
-	if result.HasFailed {
-
-		// Is the targetPort on the Service matching the containerPort in the Pod?
-		result = n.checkTargetPortMatchesContainerPort(allPods, service.Name, namespace)
-		n.tea.Send(stepMsg(result))
-
-		return nil
-	}
-
-	n.tea.Send(summaryMsg("The Service is running correctly"))
-
-	ingressName := n.askUserQuestion("Which ingress should expose this deployment?")
-
-	// Can you see a list of Backends?
-	result = n.checkCanSeeBackends(ingressName, namespace)
-	n.tea.Send(stepMsg(result))
-	if result.HasFailed {
-
-		// Are the serviceName and servicePort matching the Service?
-		result = n.checkServiceNameAndPortMatchService()
-		n.tea.Send(stepMsg(result))
-
-		return nil
+		n.tea.Send((summaryMsg(fmt.Sprintf("The Ingress %s is set up correctly", ingress.Name))))
 	}
 
 	// Can you visit the app?
@@ -224,7 +245,7 @@ func (n *Livelint) RunChecks(namespace, deploymentName string, isVerbose bool) e
 		return nil
 	}
 
-	n.tea.Send(summaryMsg("The Ingress is running correctly"))
+	n.tea.Send(summaryMsg("The Ingresses are running correctly"))
 
 	// The app should be running. Can you visit it from the public internet?
 	result = n.checkCanVisitPublicApp()
@@ -232,7 +253,6 @@ func (n *Livelint) RunChecks(namespace, deploymentName string, isVerbose bool) e
 	if result.HasFailed {
 		return nil
 	}
-
 	n.tea.Send(summaryMsg("All checks finished"))
 	return nil
 }
