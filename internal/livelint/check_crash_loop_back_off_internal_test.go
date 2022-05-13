7686eaf4bf2c50be78f4563919704cc3979c5a0b
Gabriel Koch <7779684+elessar-ch@users.noreply.github.com>
2022-05-13 12:20:19 +0200

    Add test for check for crash loop back off

---
+++ internal/livelint/check_crash_loop_back_off_internal_test.go
@@ -0,0 +1,90 @@
+package livelint
+
+import (
+	"testing"
+
+	"github.com/matryer/is"
+	apiv1 "k8s.io/api/core/v1"
+)
+
+func TestCheckCrashLoopBackOff(t *testing.T) {
+	t.Parallel()
+
+	cases := []struct {
+		it              string
+		pod             apiv1.Pod
+		containerName   string
+		expectedToFail  bool
+		expectedMessage string
+	}{
+		{
+			it:            "succeeds if the container is not waiting with reason CrashLoopBackOff",
+			containerName: "container1",
+			pod: apiv1.Pod{
+				Status: apiv1.PodStatus{
+					ContainerStatuses: []apiv1.ContainerStatus{
+						{
+							Name:  "container1",
+							State: apiv1.ContainerState{Running: &apiv1.ContainerStateRunning{}},
+						},
+					},
+				},
+			},
+			expectedToFail:  false,
+			expectedMessage: "The Pod status is not CrashLoopBackOff",
+		},
+		{
+			it:            "succeeds if there is only another container waiting with reason CrashLoopBackOff",
+			containerName: "container1",
+			pod: apiv1.Pod{
+				Status: apiv1.PodStatus{
+					ContainerStatuses: []apiv1.ContainerStatus{
+						{
+							Name:  "container1",
+							State: apiv1.ContainerState{Running: &apiv1.ContainerStateRunning{}},
+						},
+						{
+							Name:  "container2",
+							State: apiv1.ContainerState{Waiting: &apiv1.ContainerStateWaiting{Reason: "CrashLoopBackOff"}},
+						},
+					},
+				},
+			},
+			expectedToFail:  false,
+			expectedMessage: "The Pod status is not CrashLoopBackOff",
+		},
+		{
+			it:            "fails if the specified container is waiting with reason CrashLoopBackOff",
+			containerName: "container2",
+			pod: apiv1.Pod{
+				Status: apiv1.PodStatus{
+					ContainerStatuses: []apiv1.ContainerStatus{
+						{
+							Name:  "container1",
+							State: apiv1.ContainerState{Running: &apiv1.ContainerStateRunning{}},
+						},
+						{
+							Name:  "container2",
+							State: apiv1.ContainerState{Waiting: &apiv1.ContainerStateWaiting{Reason: "CrashLoopBackOff"}},
+						},
+					},
+				},
+			},
+			expectedToFail:  true,
+			expectedMessage: "The Pod status is CrashLoopBackOff",
+		},
+	}
+
+	for _, tc := range cases {
+		tc := tc
+		t.Run(tc.it, func(t *testing.T) {
+			t.Parallel()
+			is := is.New(t)
+
+			result := checkCrashLoopBackOff(tc.pod, tc.containerName)
+
+			is.Equal(result.HasFailed, tc.expectedToFail) // HasFailed
+			is.Equal(result.Message, tc.expectedMessage)  // Message
+		})
+	}
+}
