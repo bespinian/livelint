baf5f58f6b6fb2e538587a4541edd411434c63d2
John M. Hutchison <john@bespinian.io>
2022-09-23 18:23:30 +0200

    rename check tag valid to does tag exist

---
+++ internal/livelint/check_does_image_tag_exist.go
@@ -0,0 +1,32 @@
+package livelint
+
+import (
+	"fmt"
+	"strings"
+
+	apiv1 "k8s.io/api/core/v1"
+)
+
+func (n *Livelint) checkDoesImageTagExist(container apiv1.Container) CheckResult {
+	parts := strings.Split(container.Image, ":")
+
+	image := parts[0]
+	tag := "latest"
+	if len(parts) > 1 {
+		tag = parts[1]
+	}
+
+	yes := n.askUserYesOrNo(fmt.Sprintf("Does the tag %q for the image %q exist in the docker repository?", tag, image))
+
+	if !yes {
+		return CheckResult{
+			HasFailed:    true,
+			Message:      fmt.Sprintf("The tag %s for the image %s does not exist", tag, image),
+			Instructions: fmt.Sprintf("Fix the configured tag %s of the image %s to the correct value", tag, image),
+		}
+	}
+
+	return CheckResult{
+		Message: "The image tag exists",
+	}
+}
