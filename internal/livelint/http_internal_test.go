1e74237ae586c7f6632a05ee1dcb08143c77a505
Mathis Kretz <mkretz@users.noreply.github.com>
2022-11-11 17:09:02 +0100

    Add unit tests for http-based checks

---
+++ internal/livelint/http_internal_test.go
@@ -0,0 +1,15 @@
+package livelint
+
+import "net/http"
+
+type RoundTripFunc func(req *http.Request) (*http.Response, error)
+
+func (f RoundTripFunc) RoundTrip(req *http.Request) (*http.Response, error) {
+	return f(req)
+}
+
+func NewTestClient(fn RoundTripFunc) *http.Client {
+	return &http.Client{
+		Transport: fn,
+	}
+}
