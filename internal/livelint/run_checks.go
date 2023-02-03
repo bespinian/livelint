682d0fde25037bca491a63a783c61fb8d3f7e025
Paulina Grochal <ext_bes_pgrochal@xovis.com>
2023-02-03 11:17:18 +0100

    add warning field

--- internal/livelint/run_checks.go
+++ internal/livelint/run_checks.go
@@ -17,11 +17,16 @@ func (n *Livelint) RunChecks(namespace, deploymentName string, isVerbose bool) e
 		return fmt.Errorf("error getting Deployment Pods: %w", err)
 	}
 
-	// Are you hitting the ResourceQuota limits?
-	result := n.checkAreResourceQuotasHit(namespace, deploymentName)
+	// Is the number of running pods correct ?
+	result := n.CheckIsNumberOfPodsMatching(namespace, deploymentName)
 	n.ui.DisplayCheckResult(result)
 	if result.HasFailed {
-		return nil
+		// Are you hitting the ResourceQuota limits?
+		result = n.checkAreResourceQuotasHit(namespace, deploymentName)
+		n.ui.DisplayCheckResult(result)
+		if result.HasFailed {
+			return nil
+		}
 	}
 
 	// Is there any PENDING Pod?
